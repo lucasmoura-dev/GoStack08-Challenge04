7997e5014d17a27ff34b301ed90071d6fcdcefdc
Lucas Moura <lucasmoura.developer@hotmail.com>
2019-09-30 22:34:10 -0300

    chore: alterado o conteudo dos posts e comments

--- src/components/PostList/PostList.js
+++ src/components/PostList/PostList.js
@@ -9,6 +9,7 @@ import Lili from '../../assets/images/dogs/lili.jpg';
 import Rottweiler from '../../assets/images/dogs/rottweiler.jpg';
 
 class PostList extends Component {
+
   state = {
     posts: [
       {
@@ -27,14 +28,33 @@ class PostList extends Component {
               avatar: Rottweiler,
             },
             content: "A Rocketseat está sempre em busca de novos membros para o time, e geralmente ficamos de olho em quem se destaca no Bootcamp, inclusive 80% do nosso time de devs é composto por alunos do Bootcamp. Além disso, se você tem vontade de ensinar gravando vídeos e criando posts, pode me chamar no Discord! (Sério, me chamem mesmo, esse comentário é real)"
+          }
+        ]
+      },
+      {
+        id: 2,
+        author: {
+          name: 'Dog Lisboa',
+          avatar: Lili,
+        },
+        date: '04 Set 2019',
+        content: 'Fala galera, beleza\nEstou fazendo o Bootcamp GoStack da Rocketseat e está sendo muito massa! Alguém mais aí fazendo, comenta na publicação para trocarmos uma ideia.',
+        coments: [
+          {
+            id: 1,
+            author: {
+              name: 'Dog2',
+              avatar: Rottweiler,
+            },
+            content: "Também estou fazendo o Bootcamp e estou adorando! Estou no terceiro módulo sobre Node e já tenho minha API dos desafios contruída!"
           },
           {
             id: 2,
             author: {
-              name: 'Pinsher Brabo',
+              name: 'Dog3',
               avatar: Rottweiler,
             },
-            content: "A Rocketseat está sempre em busca de novos membros para o time, e geralmente ficamos de olho em quem se destaca no Bootcamp, inclusive 80% do nosso time de devs é composto por alunos do Bootcamp. Além disso, se você tem vontade de ensinar gravando vídeos e criando posts, pode me chamar no Discord! (Sério, me chamem mesmo, esse comentário é real) 2"
+            content: "Que maaaaassa! Estou pensando em me inscrever na próxima turma pra ver qual é desse Bootcamp GoStack, dizem que os devs saem de lá com super poderes!"
           }
         ]
       }
